7ecf30f277b5a88b619c528c2c81660a344ed62e
gautamranjan09 <gautamranjan96@gmail.com>
2025-02-04 11:35:07 +0530

    Add About, Blog, and Profile pages with initial content

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,8 @@
+
+const FirstBlog = () => {
+  return (
+    <div>First Blog post</div>
+  )
+}
+
+export default FirstBlog;
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,8 @@
+
+const SecondBlog = () => {
+    return (
+      <div>Second Blog post</div>
+    )
+  }
+  
+  export default SecondBlog;
